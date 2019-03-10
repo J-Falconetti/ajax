08e56d8326a26bda5fb1a42fdeb0a04db58e2ff0
J-Falconetti <joseph.falconetti@gmail.com>
2019-03-10 17:37:31 -0400

    Buttons and API request working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,31 @@ $(document).ready(function () {
 
         var swchar = ["yoda", "Chewbacca", "Luke Skywalker"];
         startbuttons();
-     
+      
+     $(".swchar").on("click", function(){
+        //  grab val from button
+        var chr=($(this).attr("data-name"));
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + chr + "&api_key=T5QXIjtQUGMUSWQFMOm1pco71dqxGs4L"
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).
+        
+        done(function (response) {
+            console.log(response)
+
+
+        })
+
+
+     })
+     $("#clicker").on("click", function(){
+        
+        var usrchr =$("#userchoice").val()
+        swchar.push(usrchr);
+        $("#userchoice").val("");
+        startbuttons()
+     })
 
         function startbuttons() {
             for (let i = 0; i < swchar.length; i++) {
@@ -14,19 +38,10 @@ $(document).ready(function () {
             }
         }
 
+
         function displaySWC() {
             var char = $(this).attr("data-name");
-            var queryURL = "https://api.giphy.com/v1/gifts/search?q=" + char + "&api_key=T5QXIjtQUGMUSWQFMOm1pco71dqxGs4L"
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).
-            
-            done(function (response) {
-                console.log(respose)
-
-
-            })
+ 
         }
     }
 
