759f467759e6300146e28621ac0c1a9693030991
J-Falconetti <joseph.falconetti@gmail.com>
2019-03-10 18:30:24 -0400

    now with adding buttons function working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,49 @@
 $(document).ready(function () {
 
-        var swchar = ["yoda", "Chewbacca", "Luke Skywalker"];
-        startbuttons();
-      
-     $(".swchar").on("click", function(){
-        //  grab val from button
-        var chr=($(this).attr("data-name"));
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + chr + "&api_key=T5QXIjtQUGMUSWQFMOm1pco71dqxGs4L"
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).
-        
-        done(function (response) {
-            console.log(response)
+    var swchar = ["yoda", "Chewbacca", "Luke Skywalker"];
+    startbuttons();
+  
+ $(".swchar").on("click", function(){
+    //  grab val from button
+    var chr=($(this).attr("data-name"));
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + chr + "&api_key=T5QXIjtQUGMUSWQFMOm1pco71dqxGs4L"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).
+    
+    done(function (response) {
+        console.log(response)
 
 
-        })
+    })
 
 
-     })
-     $("#clicker").on("click", function(){
-        
-        var usrchr =$("#userchoice").val()
-        swchar.push(usrchr);
-        $("#userchoice").val("");
-        startbuttons()
-     })
+ })
+ $("#clicker").on("click", function(){
+    
+    var usrchr =$("#userchoice").val()
+    swchar.push(usrchr);
+    $("#userchoice").val("");
+    startbuttons()
+ })
 
-        function startbuttons() {
-            for (let i = 0; i < swchar.length; i++) {
-                var btn = $("<button>");
-                btn.addClass("swchar");
-                btn.attr("data-name", swchar[i]);
-                btn.text(swchar[i]);
-                $("#buttons").append(btn);
-            }
-        }
-
-
-        function displaySWC() {
-            var char = $(this).attr("data-name");
- 
+    function startbuttons() {
+        $("#buttons").empty();
+        for (let i = 0; i < swchar.length; i++) {
+            var btn = $("<button>");
+            btn.addClass("swchar");
+            btn.attr("data-name", swchar[i]);
+            btn.text(swchar[i]);
+            $("#buttons").append(btn);
         }
     }
+
+
+    function displaySWC() {
+        var char = $(this).attr("data-name");
+
+    }
+}
 
 )
