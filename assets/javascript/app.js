6561aa72a6e86ac8a7590109795929b945dfe23f
J-Falconetti <joseph.falconetti@gmail.com>
2019-03-09 20:37:44 -0500

    improved HMTL buttons and input field are there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,33 @@
-// this is Java
+$(document).ready(function () {
+
+        var swchar = ["yoda", "Chewbacca", "Luke Skywalker"];
+        startbuttons();
+     
+
+        function startbuttons() {
+            for (let i = 0; i < swchar.length; i++) {
+                var btn = $("<button>");
+                btn.addClass("swchar");
+                btn.attr("data-name", swchar[i]);
+                btn.text(swchar[i]);
+                $("#buttons").append(btn);
+            }
+        }
+
+        function displaySWC() {
+            var char = $(this).attr("data-name");
+            var queryURL = "https://api.giphy.com/v1/gifts/search?q=" + char + "&api_key=T5QXIjtQUGMUSWQFMOm1pco71dqxGs4L"
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).
+            
+            done(function (response) {
+                console.log(respose)
+
+
+            })
+        }
+    }
+
+)
